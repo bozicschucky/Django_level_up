8741ee16fb477f328a7baf03ffbbcf1ef0b154d5
ssekito_charles <charles.ssekito@andela.com>
2018-10-04 15:06:13 +0300

    Add app urls for the polls

--- webapp/polls/views.py
+++ webapp/polls/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
 from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Question
 
 # Create your views here.
 
+
 def index(request):
-	return HttpResponse('Yoo this just worked.')
+    latest_question_list = Question.objects.order_by('-pub_date')[0:2]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request,'polls/index.html',context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/detail.html',{'question':question})
+    # return HttpResponse(f'Your Looking at question {question_id}')
+
+
+def results(request, question_id):
+    response = f'You are looking at the results {question_id}'
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You\'re voting on question {question_id}')
